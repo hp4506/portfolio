5d8801b85f9d0dbdd39683011181701c0e5b9d6d
hp4506 <hansalp06@gmail.com>
2026-06-02 14:15:05 +0530

    Update navigation bar

--- src/components/navigation.jsx
+++ src/components/navigation.jsx
@@ -3,8 +3,7 @@ import React from "react";
 export const Navigation = (props) => {
   return (
     <nav>
-      <div className="logo">HANSAL.</div>
-      <div className="nav-email">hansalp06@gmail.com</div>
+      <div className="logo">HANSAL</div>
       <div className="nav-links">
         <a href="#about">About</a>
         <a href="#portfolio">Work</a>
